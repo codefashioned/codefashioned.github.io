c7befd72aaecc000a5d6100b12f8a08cbfe02e10
Pocketbird <colin@codefashioned.com>
2015-03-27 10:39:13 -0500

    Add "xip" to browserSync.  Add typekit include.  Update serif and sans-serif font familes.

--- Gruntfile.js
+++ Gruntfile.js
@@ -310,7 +310,8 @@ module.exports = function (grunt) {
         },
         options: {
           watchTask: true,
-          proxy: '0.0.0.0:9000' // Proxy the connect:livereload server
+          proxy: '0.0.0.0:9000', // Proxy the connect:livereload server
+          xip: true
         }
       }
     }
