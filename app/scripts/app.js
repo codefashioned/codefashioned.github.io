ca3f5c312d5090b89c1c91e40be69ceb2e32fe54
Colin Carter <colin@codefashioned.com>
2015-07-13 12:40:06 -0500

    Fix jshint syntax errors.  Set "use strict" to false in jshintrc.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-  $('.js-toggle-bio').on('click', function() {
+$(document).ready(function () {
+  $('.js-toggle-bio').on('click', function () {
     $('#bio').removeClass('util-hidden--still-accessible');
     $('#tell-me-more').hide();
   });
